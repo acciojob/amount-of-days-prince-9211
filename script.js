67b02eb70f7fd6f1d7e05ec8a684fe27e6a5e94e
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2024-03-09 22:14:07 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if(year%4===0){
+		if(year%100===0){
+			if(year%400){
+				return 366;
+			} else {
+				return 365;
+			}
+		} else {
+			return 366;
+			}
+	} else { 
+		return 365;		
+	}
+}
+console.log(daysOfAYear(2022));
+console.log(daysOfAYear(2024));
+console.log(daysOfAYear(1900));
+console.log(daysOfAYear(2000));
